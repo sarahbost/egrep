33125baed1e0ad0fd9516606542ffec8b30e79df
Sarah Bost <sbost99@live.unc.edu>
2019-04-01 23:49:13 +0000

    timing out in autograder

--- src/main.rs
+++ src/main.rs
@@ -116,10 +116,10 @@ fn eval(input: &str, options: &Opt) {
 
     // no matter what options are chosen, make the NFA out of the given regex and test the input
     // for accepts based on the NFA
-    let mut regex = ".*".to_string();
-    regex.push_str(&options.paths[0]);
-    regex.push_str(".*");
-    let nfa = NFA::from(&regex).unwrap();
+   // let mut regex = ".*".to_string();
+    //regex.push_str(&options.paths[0]);
+    //regex.push_str(".*");
+    let nfa = NFA::from(&options.paths[0]).unwrap();
     if nfa.accepts(input) {
         println!("{}", input);
     }
